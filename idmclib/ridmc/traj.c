bde40dbdb59098b0bdd4ef3580abfcf60c803db2
antonio.fabio <antonio.fabio>
2007-01-06 14:03:37 +0000

    fixed svn:keywords properties to ridmc sources

--- idmclib/ridmc/traj.c
+++ idmclib/ridmc/traj.c
@@ -1,3 +1,22 @@
+/*
+ridmc: iDMC->R interface
+
+Copyright (C) 2007 Marji Lines and Alfredo Medio.
+
+Written by Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or any
+later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+Last modified: $Date$
+*/
 #include <R.h>
 #include <Rinternals.h>
 #include <stdlib.h>
