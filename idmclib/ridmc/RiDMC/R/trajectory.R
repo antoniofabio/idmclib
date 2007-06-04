bfd4b2ff4299ea5066221492094553922748d92f
antonio.fabio <antonio.fabio>
2007-06-04 16:22:59 +0000

    some additions to ctrajectory-related functions

--- idmclib/ridmc/RiDMC/R/trajectory.R
+++ idmclib/ridmc/RiDMC/R/trajectory.R
@@ -1,4 +1,4 @@
-CTrajectory <- function(idmc_model, par, var, eps, integrator=0) {
+CTrajectory <- function(idmc_model, par, var, eps, integrator=0, nsteps=0) {
 	checkModelParVar(idmc_model, par, var)
 	integrator <- as.integer(integrator)
 	if((integrator<0)||(integrator>9)) 
@@ -19,8 +19,29 @@ CTrajectory <- function(idmc_model, par, var, eps, integrator=0) {
 		.Call("ridmc_ctrajectory_step", trajectory, PACKAGE='RiDMC')
 	ans$getValue <- function()
 		.Call("ridmc_ctrajectory_getValue", trajectory, PACKAGE='RiDMC')
+	vnames <- getModelVarNames(idmc_model)
+	values <- matrix(var, 1, length(vnames))
+	colnames(values) <- vnames
+	ans$values <- values
 	class(ans) <- "idmc_ctrajectory"
-	return(ans)
+	stepCTrajectory(ans, nsteps=nsteps)
+}
+getCTrajectoryModel <- function(idmc_ctrajectory)
+	idmc_ctrajectory$model
+getCTrajectoryValues <- function(idmc_ctrajectory)
+	idmc_ctrajectory$values
+as.matrix.idmc_ctrajectory <- function(x, ...)
+	getCTrajectoryValues(x)
+
+stepCTrajectory <- function(idmc_ctrajectory, nsteps=1) {
+	vars <- getModelVarNames(getCTrajectoryModel(idmc_ctrajectory))
+	tr2 <- matrix(,nsteps, length(vars))
+	for(i in seq_len(nsteps)) {
+		idmc_ctrajectory$step()
+		tr2[i,] <- idmc_ctrajectory$getValue()
+	}
+	idmc_ctrajectory$values <- rbind(getCTrajectoryValues(idmc_ctrajectory), tr2)
+	idmc_ctrajectory
 }
 
 print.idmc_ctrajectory <- function(x, ...) {
@@ -32,3 +53,8 @@ print.idmc_ctrajectory <- function(x, ...) {
 	cat('step size: ', x$eps, '\n')
 	cat('step function: ', x$integrator, '\n')
 }
+
+#plot.idmc_ctrajectory <- function(x, y, vars=1:2, main, xlab, ylab, ...){
+#	xx <- as.matrix(x)[,vars]
+#	plot(xx, )
+#}
