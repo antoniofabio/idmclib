04a185be178db8120042e55b51148c2cb4d0f4a6
antonio.fabio <antonio.fabio>
2007-06-09 16:02:42 +0000

    added and documented bifurcation plot function

--- idmclib/ridmc/RiDMC/R/bifurcation.R
+++ idmclib/ridmc/RiDMC/R/bifurcation.R
@@ -22,12 +22,12 @@ Bifurcation <- function(idmc_model, which.var.store=1, par, var,
 
 plot.idmc_bifurcation <- function(x,y, type='p', pch=16, main, xlab, ylab, ...) {
 	y <- x$values
-	x <- rep(x$par.values, each=x$max.period)
+	x1 <- rep(x$par.values, each=x$max.period)
 	if(missing(main))
 		main <- "Bifurcation diagram"
 	if(missing(xlab))
 		xlab <- x$which.par
 	if(missing(ylab))
 		ylab <- x$which.var
-	plot(x, y, type=type, pch=pch, main=main, xlab=xlab, ylab=ylab, ...)
+	plot(x1, y, type=type, pch=pch, main=main, xlab=xlab, ylab=ylab, ...)
 }
