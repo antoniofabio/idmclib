113450379da81b844cb13c81d4a0a1388db8a508
antonio.fabio <antonio.fabio@gmail.com>
2007-06-14 22:15:53 +0200

    added version info functions. upgraded cmakefiles

---
+++ idmclib/src/version.c
@@ -0,0 +1,30 @@
+/*
+iDMC C library
+
+Adapted from iDMC, Copyright (C) 2004-2006 Marji Lines and Alfredo Medio
+Copyright (C) 2006,2007 Marji Lines and Alfredo Medio.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or any
+later version.
+
+This program is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+Last modified: $Date$
+*/
+
+int idmc_version_major() {
+	return IDMC_VERSION_MAJOR;
+}
+
+int idmc_version_minor() {
+	return IDMC_VERSION_MINOR;
+}
+
+int idmc_version_micro() {
+	return IDMC_VERSION_MICRO;
+}
