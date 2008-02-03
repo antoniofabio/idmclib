74609643c8c571dabee917d5655a27fb75a898d0
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-02-03 23:31:58 +0100

    added pkg-config file

---
+++ idmclib/idmclib.pc.cmake
@@ -0,0 +1,10 @@
+prefix=${CMAKE_INSTALL_PREFIX}
+exec_prefix=${CMAKE_INSTALL_PREFIX}
+libdir=${CMAKE_INSTALL_PREFIX}/lib
+includedir=${CMAKE_INSTALL_PREFIX}/include
+
+Name: idmclib
+Description: interactive Dynamical Model Calculator library 
+Version: ${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lidmclib ${LUA_LIBRARY} ${LUALIB_LIBRARY} ${GSL_LIBRARY} ${GSLCBLAS_LIBRARY} ${M_LIBRARY}
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include -I${LUA_INCLUDEDIR}
