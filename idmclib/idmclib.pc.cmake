dc7b4777fc439bff9171ee87311bd77af4b1f4fc
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-02-21 00:43:08 +0100

    fixed mistake in pkg-config file

--- idmclib/idmclib.pc.cmake
+++ idmclib/idmclib.pc.cmake
@@ -7,4 +7,4 @@ Name: idmclib
 Description: interactive Dynamical Model Calculator library 
 Version: @CPACK_PACKAGE_VERSION_MAJOR@.@CPACK_PACKAGE_VERSION_MINOR@.@CPACK_PACKAGE_VERSION_PATCH@
 Libs: ${libdir}/@CMAKE_STATIC_LIBRARY_PREFIX@idmclib@CMAKE_STATIC_LIBRARY_SUFFIX@ @LUA_LIBRARY@ @LUALIB_LIBRARY@ @GSL_LIBRARY@ @GSLCBLAS_LIBRARY@ @M_LIBRARY@
-Cflags: -I{includedir} -I@LUA_INCLUDEDIR@
+Cflags: -I${includedir} -I@LUA_INCLUDEDIR@
