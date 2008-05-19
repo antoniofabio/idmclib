3923998476fea6479970f5ccd4f5465e0fa80a87
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-05-19 15:54:42 +0200

    turned find_attractors into cmd line app and testing script

--- idmclib/tcl/find_attractors.tcl
+++ idmclib/tcl/find_attractors.tcl
@@ -4,29 +4,39 @@ exec tclsh "$0" ${1+"$@"}
 
 source "./init.tcl"
 
-set argv [list]
-#model file name
-lappend argv "BH-cobweb-async.lua"
-#parameters
-lappend argv "2.75 0 0.5 1.35 1 0 0"
-#x range and resolution
-lappend argv "-1.5 1.5 100"
-#y range and resolution
-lappend argv "0.0 1.0 100"
-#eps
-lappend argv 0.001
-#transient
-lappend argv 10000
-#attractor max length
-lappend argv 1000
-#number of tries
-lappend argv 1
-#x variable
-lappend argv 0
-#y variable
-lappend argv 1
-#starting point
-lappend argv "2 0.5 0.5 0 0"
+if {[lindex $argv 0] == "test"} {
+	set test 1
+	set argv [list]
+	#model file name
+	lappend argv "BH-cobweb-async.lua"
+	#parameters
+	lappend argv "2.75 0 0.5 1.35 1 0 0"
+	#x range and resolution
+	lappend argv "-1.5 1.5 100"
+	#y range and resolution
+	lappend argv "0.0 1.0 100"
+	#eps
+	lappend argv 0.001
+	#transient
+	lappend argv 10000
+	#attractor max length
+	lappend argv 1000
+	#number of tries
+	lappend argv 1
+	#x variable
+	lappend argv 0
+	#y variable
+	lappend argv 1
+	#starting point
+	lappend argv "2 0.5 0.5 0 0"
+} else {
+	set test 0
+	if {$argc < 11} {
+		puts "expected: fname \"parameters\" \"xmin xmax xres\" \"ymin ymax yres\"\
+ eps attractorLimit attractorIterations ntries xvar yvar \"startValues\""
+		return 1
+	}
+}
 
 #SET ARGUMENTS
 set fname [lindex $argv 0]
@@ -53,22 +63,32 @@ set bs [basin_multi_alloc $m $parameters $xmin $xmax $xres $ymin $ymax $yres\
 	$xvar $yvar $startValues]
 idmc_model_free $m
 
-idmc_basin_multi_find_next_attractor $bs
-set al [idmc_basin_multi_attr_head_get $bs]
-if {$al == "NULL"} {error "unexpected attractors list null pointer"}
-set attr1 [traj2list $al]
-if {[idmc_attractor_list_length $al] != 1} {error "expecting a list of length 1"}
-if {[llength $attr1] != 4} {error "expecting an attractor of length 4"}
-
-idmc_basin_multi_find_next_attractor $bs
-if {$al != [idmc_basin_multi_attr_head_get $bs]} {error "unexpected list head pointer value"}
-idmc_attractor_list_length $al
-
-idmc_basin_multi_find_next_attractor $bs
-if {$al != [idmc_basin_multi_attr_head_get $bs]} {error "unexpected list head pointer value"}
-idmc_attractor_list_length $al
-set attr2 [idmc_attractor_list_get $al 1]
-set attr2 [traj2list $attr2]
-llength $attr2
+if {$test} {
+	idmc_basin_multi_find_next_attractor $bs
+	set al [idmc_basin_multi_attr_head_get $bs]
+	if {$al == "NULL"} {error "unexpected attractors list null pointer"}
+	set attr1 [traj2list $al]
+	if {[idmc_attractor_list_length $al] != 1} {error "expecting a list of length 1"}
+	if {[llength $attr1] != 4} {error "expecting an attractor of length 4"}
+	
+	idmc_basin_multi_find_next_attractor $bs
+	if {$al != [idmc_basin_multi_attr_head_get $bs]} {error "unexpected list head pointer value"}
+	
+	idmc_basin_multi_find_next_attractor $bs
+	if {$al != [idmc_basin_multi_attr_head_get $bs]} {error "unexpected list head pointer value"}
+	set attr2 [idmc_attractor_list_get $al 1]
+	set attr2 [traj2list $attr2]
+	llength $attr2
+} else {
+	for {set i 0} {$i < $ntries} {incr i} {
+		idmc_basin_multi_find_next_attractor $bs
+	}
+	set ans [list]
+	set al [idmc_basin_multi_attr_head_get $bs]
+	for {set i 0} {$i < [idmc_attractor_list_length $al]} {incr i} {
+		lappend ans [join [traj2list $al] "\n"]
+	}
+	puts [join $ans "\n\n"]
+}
 
 idmc_basin_multi_free $bs
