eedfc47aee0a9cf76f73d7c0646604670907af0f
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-05-30 12:29:10 +0200

    cleaned out find_attractors script, added 4d model test case

--- idmclib/tcl/find_attractors.tcl
+++ idmclib/tcl/find_attractors.tcl
@@ -4,38 +4,11 @@ exec tclsh "$0" ${1+"$@"}
 
 source "./init.tcl"
 
-if {[lindex $argv 0] == "test"} {
-	set test 1
-	set argv [list]
-	#model file name
-	lappend argv "BH-cobweb-async.lua"
-	#parameters
-	lappend argv "2.75 0 0.5 1.35 1 0 0"
-	#x range and resolution
-	lappend argv "-1.5 1.5 100"
-	#y range and resolution
-	lappend argv "0.0 1.0 100"
-	#eps
-	lappend argv 0.001
-	#transient
-	lappend argv 10000
-	#attractor max length
-	lappend argv 1000
-	#number of tries
-	lappend argv 10
-	#x variable
-	lappend argv 0
-	#y variable
-	lappend argv 1
-	#starting point
-	lappend argv "2 0.5 0.5 0 0"
-} else {
-	set test 0
-	if {$argc < 11} {
-		puts "expected: fname \"parameters\" \"xmin xmax xres\" \"ymin ymax yres\"\
+if {$argc < 11} {
+	puts "only $argc arguments passed"
+	puts "expected: fname \"parameters\" \"xmin xmax xres\" \"ymin ymax yres\"\
  eps attractorLimit attractorIterations ntries xvar yvar \"startValues\""
-		return 1
-	}
+	return 1
 }
 
 #SET ARGUMENTS
@@ -56,6 +29,7 @@ set yvar [lindex $argv 9]
 set startValues [list2array [lindex $argv 10]]
 ##
 
+#LOAD MODEL, ALLOCATE RESOURCES
 set buffer [myfread $fname]
 set m [model_alloc $buffer]
 idmc_model_setGslRngSeed $m 123
@@ -63,7 +37,9 @@ set bs [basin_multi_alloc $m $parameters $xmin $xmax $xres $ymin $ymax $yres\
 	$eps $attractorLimit $attractorIterations $ntries\
 	$xvar $yvar $startValues]
 idmc_model_free $m
+##
 
+#SCAN FOR ATTRACTORS (AND STORE THEM)
 for {set i 0} {$i < $ntries} {incr i} {
 	idmc_basin_multi_find_next_attractor $bs
 }
@@ -72,11 +48,19 @@ set al [idmc_basin_multi_attr_head_get $bs]
 for {set i 0} {$i < [idmc_attractor_list_length $al]} {incr i} {
 	lappend ans [join [traj2list [idmc_attractor_list_get $al $i]] "\n"]
 }
+##
 
-if {$test} {
-	puts "Attractors found: [idmc_attractor_list_length $al]"
+#PRINT OUT RESULTS
+puts "Attractors found: [idmc_attractor_list_length $al]"
+if {[idmc_attractor_list_length $al]>0} {
+	puts -nonewline "Attractors length:"
+	for {set i 0} {$i < [idmc_attractor_list_length $al]} {incr i} {
+		puts -nonewline " [idmc_attractor_length [idmc_attractor_list_get $al $i]]"
+	}
+	puts ""
 }
+#
 
-puts [join $ans "\n\n"]
-
+#FREE ALLOCATED RESOURCES
 idmc_basin_multi_free $bs
+##
