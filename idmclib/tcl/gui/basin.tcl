8e993c23772a3a130a80285fc558303e7aa56702
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-05-31 15:47:51 +0200

    moved algorithm input in right column, added axes inputs

--- idmclib/tcl/gui/basin.tcl
+++ idmclib/tcl/gui/basin.tcl
@@ -10,6 +10,15 @@ close $fin
 set model [model_alloc $::buffer]
 wm title . "Basins of attraction - [idmc_model_name_get $model]"
 
+set ::nvar [idmc_model_var_len_get $model]
+set ::varnames [idmc_model_var_get $model]
+set ::vnlist [list]
+array set ::vids [list]
+for {set i 0} {$i < $::nvar} {incr i} {
+	lappend ::vnlist [stringArray_getitem  $::varnames $i]
+	set ::vids([stringArray_getitem  $::varnames $i]) i
+}
+
 ##Init input variables
 array set ::vEntry [list]
 array set ::pEntry [list]
@@ -44,17 +53,9 @@ proc make_input_pane {root m} {
 	ttk::frame $fr
 
 	grid [ttk::labelframe "$fr.varfr" -text "variables"] -padx 5 -pady 5 -column 0 -row 0 -sticky nsew
-	set ::nvar [idmc_model_var_len_get $m]
-	set varnames [idmc_model_var_get $m]
-	set vnlist [list]
-	array set vids [list]
-	for {set i 0} {$i < $::nvar} {incr i} {
-		lappend vnlist [stringArray_getitem  $varnames $i]
-		set vids([stringArray_getitem  $varnames $i]) i
-	}
 
 	for {set i 0} {$i < $::nvar} {incr i} {
-		grid [ttk::label "$fr.varfr.lb$i" -text [lindex $vnlist $i]] \
+		grid [ttk::label "$fr.varfr.lb$i" -text [lindex $::vnlist $i]] \
 			-column 0 -row $i -sticky ew -padx 5 -pady 5
 		grid [ttk::entry "$fr.varfr.entry$i" -textvariable ::vEntry($i)] \
 			-column 1 -row $i -sticky e -padx 5 -pady 5
@@ -69,7 +70,30 @@ proc make_input_pane {root m} {
 		grid [ttk::entry "$fr.parfr.entry$i" -textvariable ::pEntry($i)] \
 			-column 1 -row $i -sticky e -padx 5 -pady 5
 	}
-	
+
+	$c create window 10 10 -window $fr -anchor nw
+
+	update
+	$c configure -scrollregion [$c bbox all]
+	set tmp [$c bbox all]
+	$c configure -width [expr [lindex $tmp 2] - [lindex $tmp 0] ]
+}
+
+proc make_algorithm_pane {root m} {
+	set tmp root-right
+	set r [ttk::frame $root$tmp]
+	grid $r -row 0 -column 1 -sticky nsew
+	set sv "$r.sv"
+	set c "$r.c"
+	set fr "$r.fr"
+	set $sv [ttk::scrollbar $sv -orient vertical -command [list $c yview] ]
+	set $c [canvas $c -yscrollcommand [list $sv set]]
+	grid $c -column 0 -row 0 -sticky nsew
+	grid columnconfigure $r 0 -weight 1; grid rowconfigure $r 0 -weight 1
+	grid $sv -column 999 -row 0 -sticky ns
+
+	ttk::frame $fr
+
 	grid [ttk::labelframe "$fr.nitfr" -text "algorithm"] -padx 5 -pady 5 -column 0 -row 2 -sticky nsew
 	grid [ttk::label "$fr.nitfr.lbNit" -text "iterations"] \
 		-column 0 -row 0 -sticky ew -padx 5 -pady 5
@@ -80,18 +104,45 @@ proc make_input_pane {root m} {
 	grid [ttk::entry "$fr.nitfr.entryTr" -textvariable ::ntr] \
 		-column 1 -row 1 -sticky e -padx 5 -pady 5
 
-	set ::xvar-display [lindex $vnlist 0]
+	set ::xvar-display [lindex $::vnlist 0]
 	grid [ttk::label "$fr.nitfr.lbxv" -text "x axis"] \
 		-column 0 -row 2 -sticky ew -padx 5 -pady 5
 	grid [ttk::combobox "$fr.nitfr.entryXv" -state readonly -textvariable ::xvar-display] \
 		-column 1 -row 2 -sticky e -padx 5 -pady 5
-	"$fr.nitfr.entryXv" configure -values $vnlist
-	set ::yvar-display [lindex $vnlist 1]
-	grid [ttk::label "$fr.nitfr.lbyv" -text "x axis"] \
+	"$fr.nitfr.entryXv" configure -values $::vnlist
+
+	grid [ttk::label "$fr.nitfr.lbxmin" -text "x min"] \
 		-column 0 -row 3 -sticky ew -padx 5 -pady 5
-	grid [ttk::combobox "$fr.nitfr.entryYv" -state readonly -textvariable ::yvar-display] \
+	grid [ttk::entry "$fr.nitfr.entryXmin" -textvariable ::xrange(0)] \
 		-column 1 -row 3 -sticky e -padx 5 -pady 5
-	"$fr.nitfr.entryYv" configure -values $vnlist
+	grid [ttk::label "$fr.nitfr.lbxmax" -text "x max"] \
+		-column 0 -row 4 -sticky ew -padx 5 -pady 5
+	grid [ttk::entry "$fr.nitfr.entryXmax" -textvariable ::xrange(1)] \
+		-column 1 -row 4 -sticky e -padx 5 -pady 5
+	grid [ttk::label "$fr.nitfr.lbxres" -text "x resolution"] \
+		-column 0 -row 5 -sticky ew -padx 5 -pady 5
+	grid [ttk::entry "$fr.nitfr.entryXres" -textvariable ::xrange(2)] \
+		-column 1 -row 5 -sticky e -padx 5 -pady 5
+
+	set ::yvar-display [lindex $::vnlist 1]
+	grid [ttk::label "$fr.nitfr.lbyv" -text "y axis"] \
+		-column 0 -row 6 -sticky ew -padx 5 -pady 5
+	grid [ttk::combobox "$fr.nitfr.entryYv" -state readonly -textvariable ::yvar-display] \
+		-column 1 -row 6 -sticky e -padx 5 -pady 5
+	"$fr.nitfr.entryYv" configure -values $::vnlist
+
+	grid [ttk::label "$fr.nitfr.lbymin" -text "y min"] \
+		-column 0 -row 7 -sticky ew -padx 5 -pady 5
+	grid [ttk::entry "$fr.nitfr.entryYmin" -textvariable ::yrange(0)] \
+		-column 1 -row 7 -sticky e -padx 5 -pady 5
+	grid [ttk::label "$fr.nitfr.lbymax" -text "y max"] \
+		-column 0 -row 8 -sticky ew -padx 5 -pady 5
+	grid [ttk::entry "$fr.nitfr.entryYmax" -textvariable ::yrange(1)] \
+		-column 1 -row 8 -sticky e -padx 5 -pady 5
+	grid [ttk::label "$fr.nitfr.lbyres" -text "y resolution"] \
+		-column 0 -row 9 -sticky ew -padx 5 -pady 5
+	grid [ttk::entry "$fr.nitfr.entryYres" -textvariable ::yrange(2)] \
+		-column 1 -row 9 -sticky e -padx 5 -pady 5
 
 	$c create window 10 10 -window $fr -anchor nw
 
@@ -102,37 +153,12 @@ proc make_input_pane {root m} {
 }
 
 make_input_pane . $model
-grid [ttk::frame .frmBttns] -row 1 -column 0 -sticky sew
-grid [ttk::button .frmBttns.bttnDraw -text Draw -command onDraw] -row 0 -column 0 -padx 5 -pady 5 -sticky w
+make_algorithm_pane . $model
+grid [ttk::frame .frmBttns] -row 1 -column 0 -columnspan 2 -sticky sew
+grid [ttk::button .frmBttns.bttnDraw -text Draw -command onDraw] -row 0 -column 0 -padx 5 -pady 5 -sticky e
 grid columnconfigure .frmBttns 0 -weight 1; grid columnconfigure .frmBttns 1 -weight 1
 grid rowconfigure .frmBttns 0 -weight 1
 
-grid [canvas .c] -row 0 -column 1 -sticky nsew -rowspan 2
-set argv0 ""
-source gnuplot.tcl
-set ::canvas .c
-
 proc onDraw {} {
-	set ::worker [open "|tclsh traj.tcl $::argv" r+]
-	for {set i 0} {$i < $::nvar} {incr i} {
-		eval "puts $::worker $[lindex $::vEntry $i]"
-	}
-	for {set i 0} {$i < $::npar} {incr i} {
-		eval "puts $::worker $[lindex $::pEntry $i]"
-	}
-	puts $::worker $::niter
-	flush $::worker
-	gpcmd "plot '-' with lines"
-	fileevent $::worker readable {
-		set tmp [gets $::worker]
-		if [eof $::worker] {
-			gpcmd "e"
-			#fix this!
-			after 100
-			gpdraw
-			close $::worker
-		} else {
-			gpcmd $tmp
-		}
-	}
+	puts TODO
 }
