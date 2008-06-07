01a31e4808f98ccaa91162838e1703c819c2d72e
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-06-07 22:03:58 +0200

    added new basin_multi test model to repo

---
+++ idmclib/tcl/basin_test3.lua
@@ -0,0 +1,19 @@
+name = "basin test 3"
+description = "basin test 3"
+type = "D"
+parameters = {"r","s"}
+variables = {"x", "y", "z", "w"}
+
+function dist(x1, y1, x2, y2)
+	return math.abs(x1 - x2) + math.abs(y1 - y2)
+end
+
+function f(r, x, y, z, w)
+	if (dist(x, y, 0, 0) < (r-s)) then
+		return 0, 0, 0, 0
+	elseif ((dist(x, y, 0, 0) >= (r-s)) && (dist(x, y, 0, 0) <= (r+s))) then
+		return -x, -y, 0, 0
+	elseif (dist(x, y, w, z) > (r+s)) then
+		return 1/0, 0, 0, 0
+	end
+end
