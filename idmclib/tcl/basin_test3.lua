a8cd19f39fb86c690b8ad930cea38e331894e490
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-06-07 22:10:08 +0200

    fixed syntax error in basin testing model

--- idmclib/tcl/basin_test3.lua
+++ idmclib/tcl/basin_test3.lua
@@ -11,7 +11,7 @@ end
 function f(r, x, y, z, w)
 	if (dist(x, y, 0, 0) < (r-s)) then
 		return 0, 0, 0, 0
-	elseif ((dist(x, y, 0, 0) >= (r-s)) && (dist(x, y, 0, 0) <= (r+s))) then
+	elseif ((dist(x, y, 0, 0) >= (r-s)) and (dist(x, y, 0, 0) <= (r+s))) then
 		return -x, -y, 0, 0
 	elseif (dist(x, y, w, z) > (r+s)) then
 		return 1/0, 0, 0, 0
