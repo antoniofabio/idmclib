3809b7f28cd2e2ce5239dbf9a66a1c4d7f982bae
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-06-08 11:14:10 +0200

    fixed mistake in basin testing model formulation

--- idmclib/tcl/basin_test3.lua
+++ idmclib/tcl/basin_test3.lua
@@ -8,7 +8,7 @@ function dist(x1, y1, x2, y2)
 	return math.abs(x1 - x2) + math.abs(y1 - y2)
 end
 
-function f(r, x, y, z, w)
+function f(r, s, x, y, z, w)
 	if (dist(x, y, 0, 0) < (r-s)) then
 		return 0, 0, 0, 0
 	elseif ((dist(x, y, 0, 0) >= (r-s)) and (dist(x, y, 0, 0) <= (r+s))) then
