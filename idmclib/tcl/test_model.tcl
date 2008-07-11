5684014f1a986ca111cd94dda53fc2deb9314c31
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2008-07-11 13:39:05 +0200

    fixed tcl model testing script

--- idmclib/tcl/test_model.tcl
+++ idmclib/tcl/test_model.tcl
@@ -15,4 +15,4 @@ set ans [idmc_traj_trajectory_var_get $tr]
 for {set i 0} {$i < 1000} {incr i} {
 	idmc_traj_trajectory_step $tr
 }
-#stopifnot ![string compare [array2list $ans 5] "1.149432550086277 0.9497304287308597 1.0533240795128194 1.7264942102126053 0.6731701306997859"]
+stopifne [array2list $ans 5] "1.149432550086277 0.9497304287308597 1.0533240795128194 1.7264942102126053 0.6731701306997859"
