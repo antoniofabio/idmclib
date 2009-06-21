698a1154b5b96e8b1d129647ffbeb493d3b875d4
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-06-21 11:11:52 +0200

    fixed clean tgt in src makefile module

--- idmclib/src/module.mk
+++ idmclib/src/module.mk
@@ -3,7 +3,7 @@ LIB_OBJ:= $(LIB_SRC:%.c=%.o)
 LIB_HEADERS:= $(wildcard include/idmclib/*.h) include/idmclib/version.h
 
 SRC += $(LIB_SRC)
-TOCLEAN += src/libidmclib.a $(LIB_OBJ)
+TOCLEAN += src/libidmclib.a src/libidmclib.so $(LIB_OBJ)
 
 $(LIB_OBJ):%.o:%.c $(LIB_HEADERS)
 
