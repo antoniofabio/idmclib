c6c7f6d0ca851d0628cc1757142497fd849bac32
Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>
2009-07-01 00:17:30 +0200

    added unpassed testcase for basin_slow on multacc model

--- idmclib/tests/test_basin_slow.c
+++ idmclib/tests/test_basin_slow.c
@@ -1,7 +1,7 @@
 /*
 iDMC C library
 
-Copyright (C) 2007 Marji Lines and Alfredo Medio.
+Copyright (C) 2007,2009 Marji Lines and Alfredo Medio.
 
 Written by Antonio, Fabio Di Narzo <antonio.fabio@gmail.com>.
 
@@ -14,8 +14,6 @@ This program is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 General Public License for more details.
-
-Last modified: $Date$
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -117,6 +115,37 @@ static char * test_stepAll() {
 	return 0;
 }
 
+/*Test the result of a full run on the multacc model*/
+static char * test_stepMultAcc() {
+  idmc_basin_slow* basin;
+  idmc_model* model;
+  int i;
+  FILE *f;
+  int buflen;
+  char *buffer;
+  static double parms[] = {1000.0, 0.8, 10.0, 0.6, 0.5, 2.0};
+  f = fopen("multacc.lua", "r");
+  mu_assert("can't open file 'multacc.lua'", f);
+  buflen = loadFile(f, &buffer);
+  fclose(f);
+  i = idmc_model_alloc(buffer, buflen, &model);
+  free(buffer);
+  mu_assert("can't create model object", i==IDMC_OK);
+  i = idmc_basin_slow_alloc(model, &parms,
+			    0.0, 10000.0, 20,
+			    0.0, 10000.0, 20,
+			    1000, 50, 100, &basin);
+  idmc_model_free(model);
+  mu_assert("can't create basin object", i==IDMC_OK);
+  idmc_model_setGslRngSeed(basin->model, 123);
+  while(!idmc_basin_slow_finished(basin))
+    idmc_basin_slow_step(basin);
+  mu_assert("unexpected basin final result",
+	    idmc_raster_getxy(basin->raster, 9500, 500) != IDMC_BASIN_INFINITY);
+  idmc_basin_slow_free(basin);
+  return 0;
+}
+
 static int sumVector(int *vals, int nvals) {
 	int ans=0;
 	while(nvals--) ans+=vals[nvals];
@@ -132,6 +161,7 @@ static char * test_basin_slow_all() {
 	mu_run_test(test_create);
 	mu_run_test(test_init);
 	mu_run_test(test_stepAll);
+	mu_run_test(test_stepMultAcc);
 	return 0;
 }
 
